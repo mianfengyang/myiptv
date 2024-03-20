47a9ab49c9757ae4e99822c88d6e36fe34a2be33
frank <mifyang@126.com>
2024-03-20 21:02:48 +0800

    cctv13 第一财经

--- rsyncJNG.sh
+++ rsyncJNG.sh
@@ -7,9 +7,12 @@ git pull
 
 # rsync tvlist.txt to myiptv
 cd $destGitDir
-wget https://lqtv.github.io/m3u/tv.m3u -o yqiqtv.m3u
-rsync -a $sourceGitDir/tvlive/tvlive.txt $destGitDir/tvlive/tvlive.txt
+if [[ -f yqiqtv.txt ]]:then
+  rm -rf yqiqtv.txt
+fi
+wget -O yqiqtv.txt https://lqtv.github.io/m3u/tv.m3u 
+#rsync -a $sourceGitDir/tvlive/tvlive.txt $destGitDir/tvlive/tvlive.txt
 python convertToM3u.py
 git add .
-git commit -m "JNG.m3u yqiqtv.m3u"
+git commit -m "yqiqtv.m3u"
 git push
