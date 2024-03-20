ab21b54e741b2907b4a40589cd83bea06716f102
frank <mifyang@126.com>
2024-03-20 12:07:14 +0800

    JNG.m3u yqiqtv.m3u

--- rsyncJNG.sh
+++ rsyncJNG.sh
@@ -7,8 +7,9 @@ git pull
 
 # rsync tvlist.txt to myiptv
 cd $destGitDir
+wget https://lqtv.github.io/m3u/tv.m3u -o yqiqtv.m3u
 rsync -a $sourceGitDir/tvlive/tvlive.txt $destGitDir/tvlive/tvlive.txt
 python convertToM3u.py
 git add .
-git commit -m "JNG.m3u"
+git commit -m "JNG.m3u yqiqtv.m3u"
 git push
