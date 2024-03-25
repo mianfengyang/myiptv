7adec8aec61a7344e48c461fd2f1ccd3599e29c4
frank <mifyang@126.com>
2024-03-25 19:12:57 +0800

    group-title

--- itv_all_1080.py
+++ itv_all_1080.py
@@ -433,11 +433,11 @@ with open("itvlist.m3u", 'w', encoding='utf-8') as file:
                 if channel_counters[channel_name] >= result_counter:
                     continue
                 else:
-                    file.write(f"#EXTINF:-1 group-channel_name=\"收藏频道\", {channel_name}\n")
+                    file.write(f"#EXTINF:-1 group-title=\"收藏频道\", {channel_name}\n")
                     file.write(f"{channel_url}\n")
                     channel_counters[channel_name] += 1
             else:
-                file.write(f"#EXTINF:-1 group-channel_name=\"收藏频道\",{channel_name}\n")
+                file.write(f"#EXTINF:-1 group-title=\"收藏频道\",{channel_name}\n")
                 file.write(f"{channel_url}\n")
                 channel_counters[channel_name] = 1
             
